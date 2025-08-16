baa07ba89d9729428d5dbcabd674bdf9c27b88f4
cat-lover-of-doom <91356335+cat-lover-of-doom@users.noreply.github.com>
2025-08-16 13:37:06 +0200

    fixed catppuccin

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -670,9 +670,33 @@ return {
         },
     },
     {
-        "catppuccin/nvim", name = "catppuccin", priority = 1000,
-        config = function()
-        end
+        "catppuccin/nvim",
+        name = "catppuccin",
+        priority = 1000,
+        opts = {
+            flavour = "auto", -- latte, frappe, macchiato, mocha
+            transparent_background = true, -- disables setting the background color.
+            float = {
+                transparent = true,        -- enable transparent floating windows
+            },
+            styles = {                      -- Handles the styles of general hi groups (see `:h highlight-args`):
+                comments = { "italic" },    -- Change the style of comments
+                conditionals = { "italic" },
+                loops = {},
+                functions = {},
+                keywords = {},
+                strings = {},
+                variables = {},
+                numbers = {},
+                booleans = {},
+                properties = {},
+                types = { "bold" },
+                operators = {},
+            },
+        },
+        config = function(_, opts)
+            require("catppuccin").setup(opts)
+        end,
     },
     --  morta [theme]
     --  https://github.com/ssstba/morta.nvim
