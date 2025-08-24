72712d1a0508308c553afec98d8fcbe5bb64d336
cat-lover-of-doom <91356335+cat-lover-of-doom@users.noreply.github.com>
2025-08-24 12:37:42 +0200

    change mapping for find files to ff

--- lua/4-plugins.lua
+++ lua/4-plugins.lua
@@ -2096,6 +2096,14 @@ return {
                     request = "launch",
                     name = "Launch file",
                     program = "${file}", -- This configuration will launch the current file if used.
+                    pythonPath = function()
+                          local cwd = vim.fn.getcwd()
+                          if vim.fn.executable(cwd .. '/.venv/bin/python') == 1 then
+                            return cwd .. '/.venv/bin/python'
+                          else
+                            return '/usr/bin/python'  -- fallback
+                          end
+                        end,
                 },
             }
 
