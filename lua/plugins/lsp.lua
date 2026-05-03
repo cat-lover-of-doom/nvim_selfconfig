d704d9ddda686b2e69967c7a168675b2f453d908
clodman <wist.sebastian@gmail.com>
2026-05-03 17:13:53 +0200

    fixed telescope thingy

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -21,8 +21,6 @@ return {
                 caps = require("cmp_nvim_lsp").default_capabilities(caps)
             end)
 
-            local function has(mod) return pcall(require, mod) end
-
             vim.api.nvim_create_autocmd("LspAttach", {
                 group = vim.api.nvim_create_augroup("UserLspKeymaps", { clear = true }),
                 callback = function(ev)
@@ -40,25 +38,24 @@ return {
                     vim.keymap.set({ "n", "v" }, "<leader>lf", function() vim.lsp.buf.format({ async = false }) end,
                         { buffer = ev.buf, silent = true, desc = "Lsp format" })
                     vim.keymap.set("n", "<leader>lr", vim.lsp.buf.rename, { buffer = ev.buf, silent = true, desc = "Lsp rename" })
-                    -- goto / help (telescope with lsp fallback)
-                    local tel = has("telescope.builtin") and require("telescope.builtin") or nil
-                    vim.keymap.set("n", "gd", tel and function() tel.lsp_definitions() end or vim.lsp.buf.definition, { buffer = ev.buf, silent = true, desc = "Go definition" })
+                    -- goto / help
+                    vim.keymap.set("n", "gd", vim.lsp.buf.definition, { buffer = ev.buf, silent = true, desc = "Go definition" })
                     vim.keymap.set("n", "gD", vim.lsp.buf.declaration, { buffer = ev.buf, silent = true, desc = "Go declaration" })
-                    vim.keymap.set("n", "gr", tel and function() tel.lsp_references() end or vim.lsp.buf.references, { buffer = ev.buf, silent = true, desc = "Show references" })
-                    vim.keymap.set("n", "gi", tel and function() tel.lsp_implementations() end or vim.lsp.buf.implementation, { buffer = ev.buf, silent = true, desc = "Go implementation" })
-                    vim.keymap.set("n", "gT", tel and function() tel.lsp_type_definitions() end or vim.lsp.buf.type_definition, { buffer = ev.buf, silent = true, desc = "Go type definition" })
+                    vim.keymap.set("n", "gr", vim.lsp.buf.references, { buffer = ev.buf, silent = true, desc = "Show references" })
+                    vim.keymap.set("n", "gi", vim.lsp.buf.implementation, { buffer = ev.buf, silent = true, desc = "Go implementation" })
+                    vim.keymap.set("n", "gT", vim.lsp.buf.type_definition, { buffer = ev.buf, silent = true, desc = "Go type definition" })
                     vim.keymap.set("n", "gh", vim.lsp.buf.hover, { buffer = ev.buf, silent = true, desc = "Get hover info" })
                     vim.keymap.set("n", "gH", vim.lsp.buf.signature_help, { buffer = ev.buf, silent = true, desc = "Get signature_help" })
                     -- Header/Source toggle (clangd)
                     local client = vim.lsp.get_client_by_id(ev.data.client_id)
                     if client and client.name == "clangd" then
-                        vim.keymap.set("n", "<leader>lh", function()
-                            vim.lsp.buf_request(0, "clangd/switchSourceHeader", { uri = vim.uri_from_bufnr(0) }, function(err, result)
+                        vim.keymap.set("n", "gh", function()
+                            vim.lsp.buf_request(0, "textDocument/switchSourceHeader", { uri = vim.uri_from_bufnr(0) }, function(err, result)
                                 if not err and result then
                                     vim.cmd("edit " .. vim.uri_to_fname(result))
                                 end
                             end)
-                        end, { buffer = ev.buf, desc = "Alt: toggle header/source (clangd)" })
+                        end, { buffer = ev.buf, desc = "Toggle header/source (clangd)" })
                     end
                 end,
             })
